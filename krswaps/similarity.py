0d36c487e50f027cdfda0f9c536a3cfdec725ac3
kroberts8217 <KennaRoberts2029@u.northwestern.edu>
2025-09-23 16:21:02 -0500

    edit

--- krswaps/similarity.py
+++ krswaps/similarity.py
@@ -3,6 +3,15 @@ from rdkit.Chem import DataStructs, rdFMCS
 from mapchiral.mapchiral import jaccard_similarity
 
 def get_similarity(fp_1, fp_2, similarity_type: str):
+    """
+    Compute the chemical similarity between two molecules represented
+    by their fingerprints
+
+    Args:
+        fp_1: Fingerprint of molecule 1
+        fp_2: Fingerprint of molecule 2
+        similarity_type (str): Type of similarity metric to use
+    """
     if similarity_type == "dice":
         score = DataStructs.DiceSimilarity(fp_1, fp_2)
         return score
